be7df103bf50b1652140f51caace666b872fae34
Lizeth1234 <lcduenas8@misena.edu.co>
2016-11-23 09:34:27 -0500

    Update Conexion.java

--- src/Datos/Conexion.java
+++ src/Datos/Conexion.java
@@ -12,7 +12,8 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
-
+ASDFGHJKLÑÑKHFDSSA<ZXCVBNMJGFDSSDF
+SDFGHJK
 /**
  *
  * @author APRENDIZ
